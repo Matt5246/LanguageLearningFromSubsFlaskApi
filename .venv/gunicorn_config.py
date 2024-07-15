3fbee856a8eec6d12812b365ed07e38046b3289c
Matt5246 <116891485+Matt5246@users.noreply.github.com>
2024-07-15 19:03:54 +0200

    structure change

--- .venv/gunicorn_config.py
+++
@@ -1,6 +0,0 @@
-# gunicorn_config.py
-
-bind = "127.0.0.1:8000"  # Address and port to bind to
-workers = 4  # Number of worker processes
-threads = 2  # Number of threads per worker
-timeout = 120  # Timeout for worker processes
